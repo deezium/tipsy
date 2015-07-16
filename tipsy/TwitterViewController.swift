7cd24833a0fb0be8a902f0d5f6e331440ac15bec
Debarshi Chaudhuri <debarshi.chaudhuri@gmail.com>
2015-07-16 14:00:19 -0700

    Created twitter view controller

---
+++ tipsy/TwitterViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TwitterViewController.swift
+//  tipsy
+//
+//  Created by Debarshi Chaudhuri on 7/16/15.
+//  Copyright (c) 2015 Wavelength. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TwitterViewController: UIViewController {
+    
+}
