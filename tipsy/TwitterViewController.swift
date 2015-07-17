8065d95e5243a140564eb60b3ebca567e97f3ab8
Debarshi Chaudhuri <debarshi.chaudhuri@gmail.com>
2015-07-16 20:08:43 -0700

    Getting twitter data yaaaaaaay

--- tipsy/TwitterViewController.swift
+++ tipsy/TwitterViewController.swift
@@ -8,7 +8,37 @@
 
 import Foundation
 import UIKit
+import SwifteriOS
 
 class TwitterViewController: UIViewController {
     
+    var swifter : Swifter
+    
+    
+    required init(coder aDecoder: NSCoder) {
+        self.swifter = Swifter(consumerKey: "4t8tQ5YahzQtQv51QzVFQcCIM", consumerSecret: "nVwsRazhqHvZP2WNmJ0n6jlhGms3UwC46qM42fTav0UxSvU8Rd", appOnly: true)
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        println(swifter)
+        swifter.authorizeAppOnlyWithSuccess({ (accessToken, response) -> Void in
+            println("yee")
+            }, failure: { (error) -> Void in
+                println("Error Authenticating: \(error.localizedDescription)")
+        })
+    }
+    
+    @IBAction func didTapGetTwitterData(sender: AnyObject) {
+        
+        swifter.getSearchTweetsWithQuery("sup", geocode: nil, lang: nil, locale: nil, resultType: nil, count: 20, until: nil, sinceID: nil, maxID: nil, includeEntities: false, callback: nil, success: {
+            (statuses, searchMetadata) -> Void in
+                println(statuses)
+            }, failure: { (error) -> Void in
+            println("Failed to get tweets")
+        })
+       
+    }
+    
+//    let swifter = Swifter(consumerKey: "4t8tQ5YahzQtQv51QzVFQcCIM", consumerSecret: "nVwsRazhqHvZP2WNmJ0n6jlhGms3UwC46qM42fTav0UxSvU8Rd", appOnly: true)
 }
